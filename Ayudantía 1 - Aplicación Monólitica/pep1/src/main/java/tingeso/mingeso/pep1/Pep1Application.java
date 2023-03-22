ea388b1066254890c1ad342d29ea9d0c873bbe59
PodssilDev <john.serrano@usach.cl>
2023-03-22 11:59:10 -0300

    [feat]: Proyecto ejemplo 1

---
+++ Ayudantía 1 - Aplicación Monólitica/pep1/src/main/java/tingeso/mingeso/pep1/Pep1Application.java
@@ -0,0 +1,13 @@
+package tingeso.mingeso.pep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pep1Application.class, args);
+	}
+
+}
---
+++ Ayudantía 1 - Aplicación Monólitica/pep1/src/test/java/tingeso/mingeso/pep1/Pep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.mingeso.pep1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
