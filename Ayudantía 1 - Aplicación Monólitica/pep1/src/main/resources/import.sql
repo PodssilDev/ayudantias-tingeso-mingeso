6ab3a1d1971f447a1eabf9c31c4d87e31a7610e9
PodssilDev <john.serrano@usach.cl>
2023-03-22 20:31:39 -0300

    [feat]: archivo import .sql

---
+++ Ayudantía 1 - Aplicación Monólitica/pep1/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO proveedores(codigo,nombre,categoria,retencion) VALUES ("01537", "QUISPE S. ALCIDES", "A", "No");
+INSERT INTO proveedores(codigo,nombre,categoria,retencion) VALUES ("02994", "BELLO R. PHILIP", "B", "Si");
+INSERT INTO proveedores(codigo,nombre,categoria,retencion) VALUES ("013312", "ROMAN A. PABLO", "C", "No");
