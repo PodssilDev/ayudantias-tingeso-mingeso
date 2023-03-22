bac2b602f41aa16c2eda24077e83358efce228f7
PodssilDev <john.serrano@usach.cl>
2023-03-22 11:51:39 -0300

    [feat]: Material ayudantía 1

---
+++ Ayudantía 1 - Aplicación Monólitica/demo/src/test/java/tingeso/pep1/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.pep1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
