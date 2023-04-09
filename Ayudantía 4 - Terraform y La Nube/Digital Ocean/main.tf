a80f8be30ed27e311629082e4d2f2b9f55d52ae6
John Serrano <91446330+PodssilDev@users.noreply.github.com>
2023-04-09 14:01:24 -0400

    [feat]: main.tf DIgital Ocean

---
+++ Ayudantía 4 - Terraform y La Nube/Digital Ocean/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = "dop_v1_e255103795b6740c0a7c8054f6e7605e7a086744630c21b09a7267606d9337b7"
+}
+
+# Create a new Web Droplet in the nyc2 region
+resource "digitalocean_droplet" "web" {
+  image  = "ubuntu-18-04-x64"
+  name   = "web-1"
+  region = "nyc3"
+  size   = "s-1vcpu-2gb"
+  ssh_keys = ["5f:8d:ca:ad:73:85:a1:b1:34:c2:16:08:8a:8d:32:48"]
+}
